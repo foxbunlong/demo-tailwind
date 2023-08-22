78fd8062e7f34ea2d5e82ea6f7d6a50875ce8762
Long Thay <thaybunlong20@yahoo.com>
2023-08-22 09:12:01 +0700

    feat: custom size and padding. Add inline styling (plugin)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,17 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-white bg-green-600 border-4 border-green-800">
-        Hello World!
+        Custom color
       </h1>
 
       <h1 className="text-primary-100">Hello World!</h1>
       <h1 className="text-primary-932">Hello World!</h1>
+
+      <h1 className="text-white bg-green-600 border-4 border-green-800">
+        Custom size
+      </h1>
+      <p className="text-xs lg:text-lg mb-1 bg-[#d0ff45]">Hello long sentence</p>
+      <p>Another long sentence</p>
     </div>
   );
 }
